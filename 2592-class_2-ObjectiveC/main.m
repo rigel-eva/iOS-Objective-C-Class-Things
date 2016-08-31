59b481ffb2d5f0bc6c0306554c2a345298fed2fc
sir_rigel <sir.rigel@gmail.com>
2016-08-31 18:19:42 -0500

    Initial Commit

---
+++ 2592-class_2-ObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2592-class_2-ObjectiveC
+//
+//  Created by Alex Maguire on 8/31/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
