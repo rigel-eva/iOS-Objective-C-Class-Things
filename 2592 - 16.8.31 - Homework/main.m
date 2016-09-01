2d548240bcace45431158600a7d8245a6bacaeae
sir_rigel <sir.rigel@gmail.com>
2016-09-01 13:41:03 -0500

    Initial Commit

---
+++ 2592 - 16.8.31 - Homework/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2592 - 16.8.31 - Homework
+//
+//  Created by Alex Maguire on 9/1/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
