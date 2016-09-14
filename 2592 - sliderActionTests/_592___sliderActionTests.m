1c9e7acd0264595bf0bdb0edebf6bba0cb76b77a
student <student@dyn225-22-21.cod.edu>
2016-09-14 18:57:59 -0500

    Initial Commit

---
+++ 2592 - sliderActionTests/_592___sliderActionTests.m
@@ -0,0 +1,39 @@
+//
+//  _592___sliderActionTests.m
+//  2592 - sliderActionTests
+//
+//  Created by student on 9/14/16.
+//  Copyright © 2016 sir_rigel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _592___sliderActionTests : XCTestCase
+
+@end
+
+@implementation _592___sliderActionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
