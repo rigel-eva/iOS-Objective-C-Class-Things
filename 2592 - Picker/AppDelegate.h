ee4fc0b7411731bc11d805e01f5b6ce5f80563e7
sir_rigel <sir.rigel@gmail.com>
2016-09-14 20:13:47 -0500

    Initial Commit

---
+++ 2592 - Picker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2592 - Picker
+//
+//  Created by student on 9/14/16.
+//  Copyright © 2016 sir_rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2592 - Picker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2592 - Picker
+//
+//  Created by student on 9/14/16.
+//  Copyright © 2016 sir_rigel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
