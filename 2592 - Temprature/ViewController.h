ac36f8c55f77aa1f8319dae20d92359a4dad1181
sir_rigel <sir.rigel@gmail.com>
2016-09-14 20:36:56 -0500

    Initial Commit

---
+++ 2592 - Temprature/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2592 - Temprature
+//
+//  Created by student on 9/14/16.
+//  Copyright © 2016 sir_rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
