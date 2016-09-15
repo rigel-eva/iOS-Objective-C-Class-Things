8fba15a1717770470a2fe7e5de5b0b98500ffeb3
sir_rigel <sir.rigel@gmail.com>
2016-09-14 20:19:16 -0500

    added skeleton of a picker

--- 2592 - Picker/ViewController.h
+++ 2592 - Picker/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+<UIPickerViewDelegate, UIPickerViewDataSource>{
+    IBOutlet UIPickerView *picker;
+}
+@property (nonatomic, retain) UIPickerView *picker;
 @end
 
