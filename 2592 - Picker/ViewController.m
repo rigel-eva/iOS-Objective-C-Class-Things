c958b24afdb684f59e4b604926da5070ea4b2959
sir_rigel <sir.rigel@gmail.com>
2016-09-14 20:35:37 -0500

    And now the picker view has data in it, and can return data, sweet!

--- 2592 - Picker/ViewController.m
+++ 2592 - Picker/ViewController.m
@@ -14,7 +14,7 @@
 
 @implementation ViewController
 @synthesize picker;
-static NSString *pd[3]={@"One",@"Two",@"Three"};
+static NSString *pd[3]={@"One",@"Two",@"Three"};//Data for the dataSource gods
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,5 +24,16 @@ static NSString *pd[3]={@"One",@"Two",@"Three"};
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return 3;
+}
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return pd[row];
+}
+-(void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    NSLog(@"didSelectRow: %li, inComponent:%li",row, component);
+}
 @end
