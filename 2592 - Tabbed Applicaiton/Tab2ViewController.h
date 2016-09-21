b0e9de22baf36209bc55c773b95eb56b91fc1eaa
sir_rigel <sir.rigel@gmail.com>
2016-09-21 18:21:54 -0500

    Added Framework for Tab2ViewController

---
+++ 2592 - Tabbed Applicaiton/Tab2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Tab2ViewController.h
+//  2592 - Tabbed Applicaiton
+//
+//  Created by Alex Maguire on 9/21/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tab2ViewController : UIViewController
+
+@end
---
+++ 2592 - Tabbed Applicaiton/Tab2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Tab2ViewController.m
+//  2592 - Tabbed Applicaiton
+//
+//  Created by Alex Maguire on 9/21/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import "Tab2ViewController.h"
+
+@interface Tab2ViewController ()
+
+@end
+
+@implementation Tab2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
