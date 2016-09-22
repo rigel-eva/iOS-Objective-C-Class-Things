feb775364684454965313548d1453593707f1353
sir_rigel <sir.rigel@gmail.com>
2016-09-21 20:56:34 -0500

    So I think everything is set up for this...

--- 2592-pageView/ViewController.h
+++ 2592-pageView/ViewController.h
@@ -8,8 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+    IBOutlet UIPageViewControl *pageView;
+    IBOutlet UIImageView *imageView0;
+    IBOutlet UIImageView *imageView1;
+    UIImageView *tempImageView, *bgImageView;
+}
+@property (nonatomic,retain) UIPageControl *PageView;
+@property (nonatomic, retain) UIImageView *imageView0, *imageView1,*tempImageView, *bgImageView;
 
 @end
 
