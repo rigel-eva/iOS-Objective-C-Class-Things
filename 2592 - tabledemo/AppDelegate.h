92aae52395c8a2b5201e930f38e520afafc8321c
sir_rigel <sir.rigel@gmail.com>
2016-09-28 18:46:38 -0500

    Initial Commit

---
+++ 2592 - tabledemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2592 - tabledemo
+//
+//  Created by Alex Maguire on 9/28/16.
+//  Copyright © 2016 Alex Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2592 - tabledemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2592 - tabledemo
+//
+//  Created by Alex Maguire on 9/28/16.
+//  Copyright © 2016 Alex Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
