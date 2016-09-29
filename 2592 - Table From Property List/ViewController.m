2412dde34aebeb203859df7ff2783d4fac0bac95
sir_rigel <sir.rigel@gmail.com>
2016-09-28 19:45:33 -0500

    Initial Commit

---
+++ 2592 - Table From Property List/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2592 - Table From Property List
+//
+//  Created by Alex Maguire on 9/28/16.
+//  Copyright © 2016 Alex Maguire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 2592 - Table From Property List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2592 - Table From Property List
+//
+//  Created by Alex Maguire on 9/28/16.
+//  Copyright © 2016 Alex Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
