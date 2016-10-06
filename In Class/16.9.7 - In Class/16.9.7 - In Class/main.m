a59cb542e37fbd346bd6fb25617f1cb283968cd1
sir_rigel <sir.rigel@gmail.com>
2016-10-05 22:52:28 -0500

    Preping to move to new repo

---
+++ In Class/16.9.7 - In Class/16.9.7 - In Class/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  16.9.7 - In Class
+//
+//  Created by Alex Maguire on 9/7/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
