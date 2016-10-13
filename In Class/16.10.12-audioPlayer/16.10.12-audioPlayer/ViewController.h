c0ee17c2146f102b25e226fe5c0165767a6de094
sir_rigel <sir.rigel@gmail.com>
2016-10-12 19:24:30 -0500

    added finalProject, audioplayer
    added skeleton of final project

---
+++ In Class/16.10.12-audioPlayer/16.10.12-audioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16.10.12-audioPlayer
+//
+//  Created by Alex Maguire on 10/12/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
