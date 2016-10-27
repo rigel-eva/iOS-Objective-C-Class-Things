29d0c4eed7d6550c9e706833684db78c1acdb362
sir_rigel <sir.rigel@gmail.com>
2016-10-26 19:48:09 -0500

    New project, so ... gestures

---
+++ In Class/16.10.26 - gesture/16.10.26 - gesture/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  16.10.26 - gesture
+//
+//  Created by Alex Maguire on 10/26/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UIImageView *imageView;
+}
+@property (nonatomic,retain) UIImageView *imageView;
+@end
+
