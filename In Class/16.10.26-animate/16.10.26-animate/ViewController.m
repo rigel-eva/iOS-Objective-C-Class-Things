e62dda7d4d6e024716139c96d7f95590cfa41e67
sir_rigel <sir.rigel@gmail.com>
2016-10-26 19:35:07 -0500

    Um ... I think this is slightly less choppy?

--- In Class/16.10.26-animate/16.10.26-animate/ViewController.m
+++ In Class/16.10.26-animate/16.10.26-animate/ViewController.m
@@ -29,8 +29,9 @@
     [UIView beginAnimations:@"myOwn" context:nil];
     [UIView setAnimationDuration:slider.value];
     [UIView setAnimationCurve:UIViewAnimationCurveLinear];
-    //imageView.transform = CGAffineTransformMakeTranslation(translation.x, translation.y);
-    /*//set up next timer call
+    CGAffineTransform transform=CGAffineTransformIdentity;
+    transform=CGAffineTransformTranslate(transform, translation.x, translation.y);
+    //set up next timer call
     translation.x=translation.x+delta.x;
     translation.y=translation.y+delta.y;
     if((imageView.center.x+translation.x>self.view.bounds.size.width-ballRadius)||(imageView.center.x+translation.x<ballRadius)){
@@ -38,22 +39,15 @@
     }
     if((imageView.center.y+translation.y>self.view.bounds.size.height-ballRadius)||(imageView.center.y+translation.y<ballRadius)){
         delta.y= -delta.y;
-    }*/
-    imageView.transform=CGAffineTransformMakeRotation(angle);
+    }
+    transform=CGAffineTransformRotate(transform, angle);
+    imageView.transform=transform;
     [UIView commitAnimations];
     angle+=0.02;
     //full rotation is exactly waht you expect, 2 pi or 2 *3.1415926(using M_PI for ease of use...)
     if(angle>M_PI*2){
         angle=0;
     }
-    imageView.center=CGPointMake(imageView.center.x+delta.x, imageView.center.y +delta.y);
-     if(imageView.center.x>self.view.bounds.size.width -ballRadius||
-        imageView.center.x>self.view.bounds.size.width-ballRadius){
-         delta.x=-delta.x;
-     }
-     if(imageView.center.y>self.view.bounds.size.height-ballRadius||imageView.center.y<ballRadius){
-         delta.y=-delta.y;
-     }
     
 }
 -(IBAction)sliderMoved:(id)sender{
