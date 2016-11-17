eed9dfa6380b869479b3c479511e12905f81707c
sir_rigel <sir.rigel@gmail.com>
2016-11-16 19:28:33 -0600

    This is how we XML apparently …

---
+++ In Class/16.11.16 - rssViewer/16.11.16 - rssViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  16.11.16 - rssViewer
+//
+//  Created by Alex Maguire on 11/16/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ In Class/16.11.16 - rssViewer/16.11.16 - rssViewer/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  16.11.16 - rssViewer
+//
+//  Created by Alex Maguire on 11/16/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController <NSXMLParserDelegate>{
+    NSMutableArray *rssList;
+    
+}
+
+
+@end
+
