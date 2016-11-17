5737c16a8e7cafba2132dea0fc44a1fc806d167d
sir_rigel <sir.rigel@gmail.com>
2016-11-17 12:53:08 -0600

    So, this is the initial stuff to get the table to work …

---
+++ Homework/HW4 - Table ex SQL/HW4 - Table ex SQL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW4 - Table ex SQL
+//
+//  Created by Alex Maguire on 11/17/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homework/HW4 - Table ex SQL/HW4 - Table ex SQL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW4 - Table ex SQL
+//
+//  Created by Alex Maguire on 11/17/16.
+//  Copyright © 2016 Sir Rigel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
